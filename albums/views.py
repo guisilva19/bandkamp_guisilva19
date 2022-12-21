fdb5c11d9da35e226e8d9f90dc8c1ef789a3247a
Guilherme Silva <2003silvagui@gmail.com>
2022-12-21 01:13:55 -0300

    finish

---
+++ albums/views.py
@@ -0,0 +1,14 @@
+from .models import Album
+from .serializers import AlbumSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics
+
+
+class AlbumView(generics.ListCreateAPIView, PageNumberPagination):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    serializer_class = AlbumSerializer
+    queryset = Album.objects.all()
---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=127, unique=True)
+    email = models.EmailField(max_length=127, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
